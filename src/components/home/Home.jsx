d65ae2f31a4c213381fe9dd551e9c61bff497c32
josueventura07 <josueandresventura@gmail.com>
2023-04-22 00:07:01 -0400

    feat: josue

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,16 +7,15 @@ import './styles/home.css'
 const Home = () => {
   return (
     <div className='pokedex__body'>
-          <div className='pokedex'>
-      <img className='pokedex_img' src="/images/home/pokedex.png" alt="img" />
-      <header className='pokedex_header'>
-        <h2 className='pokedex_subtitle'>Hello Trainer!</h2>
-        <p className='pokedex_text'>Give me your name, to see the pokedex</p>
-      </header>
-      <FormHome className='pokedex_form' />
-      
-    </div>
-    <img className='pokedex_img2' src='./images/personajes/personaje1.png' alt="img" />
+      <div className='pokedex'>
+        <img className='pokedex_img' src="/images/home/pokedex.png" alt="img" />
+        <header className='pokedex_header'>
+          <h2 className='pokedex_subtitle'>Hello Trainer!</h2>
+          <p className='pokedex_text'>Give me your name, to see the pokedex</p>
+        </header>
+        <FormHome className='pokedex_form' />
+      </div>
+        <img className='pokedex_img2' src='./images/personajes/personaje1.png' alt="img" />
     </div>
   )
 }
